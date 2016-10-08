e398dd8195492843fe45a7dbbe66f4e3ee0ac340
Michael Georgoulopoulos <mgeorgoulopoulos@gmail.com>
2016-10-09 01:43:12 +0300

    Added some basic definitions for the playfield

--- Playfield.hs
+++ Playfield.hs
@@ -8,18 +8,24 @@ module Playfield () where
 data Cell = Empty | Full deriving (Eq)
 
 -- Then, we define a row of the playfield as a list of cells
-data Row = Row [Cell]
+data Row = RowOfCells [Cell]
+
+-- Creates an empty Row
+emptyRow = RowOfCells (replicate 10 Empty)
 
 -- Function stubs that tell if a row is empty or full
-emptyRow :: Row -> Bool
-emptyRow r = undefined
+isRowEmpty :: Row -> Bool
+isRowEmpty r = undefined
 
-fullRow :: Row -> Bool
-fullRow r = undefined
+isRowFull :: Row -> Bool
+isRowFull r = undefined
 
 
 -- Finally, our playfield type that we'll call a "Well".
 -- This is a list of Rows
-data Well = Well [Row]
+data Well = WellOfRows [Row]
 
+-- Creates an empty Well
+emptyWell = WellOfRows (replicate 22 emptyRow)
 
+-- Our playfield will be stored differently, partially as an exercise and partially to be able to store the rendered pieces with different colours in the playfield
