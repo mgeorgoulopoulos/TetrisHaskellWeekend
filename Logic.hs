148c63c9ba1e537687744baffd99fa7c2eca0f30
Michael Georgoulopoulos <mgeorgoulopoulos@gmail.com>
2016-10-10 17:00:28 +0300

    Unity-style update with time and deltaTime in the state

--- Logic.hs
+++ Logic.hs
@@ -10,5 +10,10 @@ import Graphics.Gloss.Interface.Pure.Game -- for Event
 handleEvent :: Event -> State -> State
 handleEvent _ s = s
 
+-- I want a unity style with time and deltaTime in the state struct.
+-- So I'm just going to diff the times here and do the update in another func
 updateGameState :: Float -> State -> State
-updateGameState t s = s
+updateGameState t s = unityStyleUpdate (s {time = t, deltaTime = t - (time s)})
+
+unityStyleUpdate :: State -> State
+unityStyleUpdate s = s
--- State.hs
+++ State.hs
@@ -1,15 +1,19 @@
 -- Game State
 
-module State(State, well, initialGameState) where
+module State(State(..), initialGameState) where
 
 import Playfield
 import Piece
 
 data State = State
     { well :: Well
+    , time :: Float
+    , deltaTime :: Float
     } deriving (Show)
 
 initialGameState :: State
 initialGameState = State
     { well = renderPiece tetrominoT (0, -10) emptyWell
+    , time = 0
+    , deltaTime = 0
     }
